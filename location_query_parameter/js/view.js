979b72ecd60bf1207188f88aae2a63769d27513a
Takano, Yasushi <yasushi-R.takano@sonymusic.co.jp>
2016-10-12 17:00:05 +0900

    クエリが無かった場合のエラーを解消

--- location_query_parameter/js/view.js
+++ location_query_parameter/js/view.js
@@ -1,39 +1,60 @@
-// URLを取得して「?]で分割「&」でも分割
-var url   = location.href;
-params    = url.split('?');
-paramms   = params[1].split('&');
+$(function(){
 
-// パラメータ用の配列を用意
-var paramArray = [];
+  // URLを取得
+  var url   = location.href;
 
-// 配列にパラメータを格納
-for ( i = 0; i < paramms.length; i++ ) {
+  // URLにパラメータ「?]または「&」があったら分割
+  if ( url.indexOf( '?' || '&' ) != -1 ) {
 
-    neet = paramms[i].split('=');
-    paramArray.push(neet[0]);
-    paramArray[neet[0]] = neet[1];
+    params    = url.split('?');
+    paramms   = params[1].split('&');
 
-}
+  } else {
 
-// パラメータによって処理を分岐
-if ( paramArray['viewtype'] == 'viewtype01') {
+    // パラメータなければdefaultを表示
+    console.log('It does not fit anything. default');
+    $('.default').fadeIn('slow');
+    return;
 
-  // ?viewtype=viewtype01
-  alert('success! viewtype01');
+  }
 
-} else if ( paramArray['viewtype'] == 'viewtype02') {
+  // パラメータ用の配列を用意
+  var paramArray = [];
 
-  // ?viewtype=viewtype02
-  alert('success! viewtype02');
+  // 配列にパラメータを格納
+  for ( i = 0; i < paramms.length; i++ ) {
 
-} else if ( paramArray['default'] == 'default') {
+      neet = paramms[i].split('=');
+      paramArray.push(neet[0]);
+      paramArray[neet[0]] = neet[1];
 
-  // ?viewtype=default
-  alert('success! default');
+  }
 
-} else {
+  // パラメータによって処理を分岐
+  if ( paramArray['viewtype'] === 'viewtype01') {
 
-  // クエリ適合無し(クエリ無し)
-  alert('It does not fit anything. default');
+    // ?viewtype=viewtype01
+    console.log('success! viewtype01');
+    $('.viewtype01').fadeIn('slow');
 
-}
+  } else if ( paramArray['viewtype'] === 'viewtype02') {
+
+    // ?viewtype=viewtype02
+    console.log('success! viewtype02');
+    $('.viewtype02').fadeIn('slow');
+
+  } else if ( paramArray['viewtype'] === 'default') {
+
+    // ?viewtype=default
+    console.log('success! default');
+    $('.default').fadeIn('slow');
+
+  } else {
+
+    // クエリ適合無し(クエリ無し)
+    console.log('It does not fit anything. default');
+    $('.default').fadeIn('slow');
+
+  }
+
+});
